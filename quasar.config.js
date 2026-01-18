0ff5f680f017d89eb1bbd5b0c7b019c4a06d0693
Taewoo Lee <sai6321@naver.com>
2026-01-18 16:48:53 +0900

    vue-router 최신 버전으로 업데이트

--- quasar.config.js
+++ quasar.config.js
@@ -40,7 +40,7 @@ export default defineConfig((/* ctx */) => {
     build: {
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
-        node: 'node20'
+        node: 'node22'
       },
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
